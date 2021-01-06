36646ea2d6ee0f7a780d37ef2b25159e6bc0b5bb
Bartosz Drzewiecki <s15451@pjwstk.edu.pl>
2021-01-06 22:45:02 +0100

    adding some stuff

--- wp-content/plugins/favourite-recipes/favourite-recipes.php
+++ wp-content/plugins/favourite-recipes/favourite-recipes.php
@@ -1,1 +1,17 @@
 <?php
+/**
+ * @package  FavouriteRecipes
+ */
+
+/*
+Plugin Name: Favourite Recipes
+Description: Plugin used to store favourite recipes of users in the database.
+Version: 1.0
+Author: Grupa C
+License: GPLv2
+Text Domain: favourite-recipes
+ */
+
+defined('ABSPATH') or die('Don\'t do this');
+
+define( 'FAVOURITE_INGREDIENTS_BASE_PATH', __DIR__ );
